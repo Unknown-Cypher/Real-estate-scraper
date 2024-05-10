4e66f8809b0ec913a60ab9734c3b78a41d6d4aef
Chandrahas <chandugumidelli@gmail.com>
2024-05-10 16:29:12 +0530

    10-05-2024

---
+++ homescraper/spiders/homespider.py
@@ -0,0 +1,50 @@
+import scrapy
+import re
+
+class HomespiderSpider(scrapy.Spider):
+    name = "homespider"
+    allowed_domains = ["southernimpressionhomes.com"]
+    start_urls = ["https://southernimpressionhomes.com/properties/"]
+    output_data = []
+
+    def parse(self, response):
+        # Extracting links to individual home pages
+        home_links = response.css('a.view_detail::attr(href)').extract()
+        del home_links[1::2]
+        for link in home_links:
+            yield response.follow(link,callback = self.parse_home)
+        
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not '#':
+            yield response.follow(next_page,callback=self.parse)
+
+    
+    
+
+    def parse_home(self, response):
+        def clean_html(data):
+          clean = re.compile('<.*?>')
+          data = re.sub(clean,'',data)
+          return data
+        data = response.css('div.rows').extract()
+        data = [clean_html(x) for x in data]
+        features = response.css('div.wpl-column.rows.feature').extract()
+        features = [clean_html(x) for x in features]
+        basic_details = response.css('div.wpl_category_1 div.wpl_prp_show_detail_boxes_cont div').extract()
+        basic_details = [clean_html(x) for x in basic_details]
+        name = response.css('h1.title_text::text').get()
+        video_link = response.css('li.wpl_videos_video iframe::attr(src)').get()
+        images = response.xpath("//div[@id = 'wpl_gallery_wrapper-3']//li/@data-src").extract()
+        blueprint = response.xpath("//div[@id = 'wpl_gallery_wrapper-101']//li/@data-src").extract()
+        address_map = {entry.split(':')[0].strip().replace(' ', '_'): entry.split(':')[1] for entry in data[len(features):-len(basic_details)]}
+        address_map ['latitude'] = float(response.text.split('googlemap_lt":')[1].split(',')[0])
+        address_map ['longitude'] = float(response.text.split('googlemap_ln":')[1].split(',')[0])
+        yield{
+            'name':name,
+            'basic_details':{entry.split(':')[0].strip().replace(' ', '_'): entry.split(':')[1] for entry in basic_details},
+            'address_map':address_map,
+            'features':features,
+            'images':images,
+            'video_link':video_link,
+            'blueprint':blueprint
+          }
