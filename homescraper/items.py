cfa30c2e6cdc8c5f3946e5f6e824bb3c786fd009
Chandrahas <chandugumidelli@gmail.com>
2024-05-20 17:57:58 +0530

    converted python object to xml

--- homescraper/items.py
+++ homescraper/items.py
@@ -9,50 +9,90 @@ class HomeItem(scrapy.Item):
     blueprint = scrapy.Field()
 
 class CoopRate(scrapy.Item):
-    honors_coop_rate = scrapy.Field(default=True)
-    coop_rate_pct = scrapy.Field(default=None)
+    honors_coop_rate = scrapy.Field(default="True")
+    coop_rate_pct = scrapy.Field(default="")
+
+    def initialize_defaults(self):
+        for field, metadata in self.fields.items():
+            if 'default' in metadata:
+                self.setdefault(field, metadata['default'])
 
 class Promotion(scrapy.Item):
-    promo_id = scrapy.Field(default=None)
-    promo_type = scrapy.Field(default=None)
-    promo_headline = scrapy.Field(default=None)
-    promo_description = scrapy.Field(default=None)
-    promo_url = scrapy.Field(default=None)
-    promo_start_date = scrapy.Field(default=None)
-    promo_end_date = scrapy.Field(default=None)
+    promo_id = scrapy.Field(default="")
+    promo_type = scrapy.Field(default="")
+    promo_headline = scrapy.Field(default="")
+    promo_description = scrapy.Field(default="")
+    promo_url = scrapy.Field(default="")
+    promo_start_date = scrapy.Field(default="")
+    promo_end_date = scrapy.Field(default="")
+
+    def initialize_defaults(self):
+        for field, metadata in self.fields.items():
+            if 'default' in metadata:
+                self.setdefault(field, metadata['default'])
 
 class SubImage(scrapy.Item):
-    sub_image_url = scrapy.Field(default=None)
-    sub_image_caption = scrapy.Field(default=None)
-    sub_image_url_content_type = scrapy.Field(default=None)
-    sub_image_order_by = scrapy.Field(default=None)
-    main_flag = scrapy.Field(default=None)
+    sub_image_url = scrapy.Field(default="")
+    sub_image_caption = scrapy.Field(default="")
+    sub_image_url_content_type = scrapy.Field(default="")
+    sub_image_order_by = scrapy.Field(default="")
+    main_flag = scrapy.Field(default="")
+
+    def initialize_defaults(self):
+        for field, metadata in self.fields.items():
+            if 'default' in metadata:
+                self.setdefault(field, metadata['default'])
 
 class SubVideo(scrapy.Item):
-    sub_video_url = scrapy.Field(default=None)
-    sub_video_caption = scrapy.Field(default=None)
-    sub_video_url_content_type = scrapy.Field(default=None)
-    sub_video_order_by = scrapy.Field(default=None)
-    main_flag = scrapy.Field(default=None)
+    sub_video_url = scrapy.Field(default="")
+    sub_video_caption = scrapy.Field(default="")
+    sub_video_url_content_type = scrapy.Field(default="")
+    sub_video_order_by = scrapy.Field(default="")
+    main_flag = scrapy.Field(default="")
+
+    def initialize_defaults(self):
+        for field, metadata in self.fields.items():
+            if 'default' in metadata:
+                self.setdefault(field, metadata['default'])
 
 class SubdivisionFlyer(scrapy.Item):
-    subdivision_flyer_image_url = scrapy.Field(default=None)
-    subdivision_flyer_description = scrapy.Field(default=None)
+    subdivision_flyer_image_url = scrapy.Field(default="")
+    subdivision_flyer_description = scrapy.Field(default="")
+
+    def initialize_defaults(self):
+        for field, metadata in self.fields.items():
+            if 'default' in metadata:
+                self.setdefault(field, metadata['default'])
 
 class PropertyElevationImage(scrapy.Item):
-    property_elevation_image_url = scrapy.Field(default=None)
-    property_elevation_image_description = scrapy.Field(default=None)
+    property_elevation_image_url = scrapy.Field(default="")
+    property_elevation_image_description = scrapy.Field(default="")
+
+    def initialize_defaults(self):
+        for field, metadata in self.fields.items():
+            if 'default' in metadata:
+                self.setdefault(field, metadata['default'])
 
 class PropertyExteriorInteriorImage(scrapy.Item):
-    property_interior_image_url = scrapy.Field(default=None)
-    property_interior_image_description = scrapy.Field(default=None)
+    property_interior_image_url = scrapy.Field(default="")
+    property_interior_image_description = scrapy.Field(default="")
+
+    def initialize_defaults(self):
+        for field, metadata in self.fields.items():
+            if 'default' in metadata:
+                self.setdefault(field, metadata['default'])
 
 class PropertyFloorPlanImage(scrapy.Item):
-    property_floor_plan_image_url = scrapy.Field(default=None)
-    property_floor_plan_image_description = scrapy.Field(default=None)
+    property_floor_plan_image_url = scrapy.Field(default="")
+    property_floor_plan_image_description = scrapy.Field(default="")
+
+    def initialize_defaults(self):
+        for field, metadata in self.fields.items():
+            if 'default' in metadata:
+                self.setdefault(field, metadata['default'])
 
 class SubdivisionProperty(scrapy.Item):
-    property_id = scrapy.Field(default=None)
+    property_id = scrapy.Field(default="")
     property_address = scrapy.Field()
     property_zip = scrapy.Field()
     property_price = scrapy.Field()
@@ -61,54 +101,59 @@ class SubdivisionProperty(scrapy.Item):
     property_class_code = scrapy.Field(default='PROPERTY')
     property_status_id = scrapy.Field(default='AV')
     property_stage_id = scrapy.Field()
-    completion_date = scrapy.Field(default = None)
+    completion_date = scrapy.Field(default = "")
     property_type_id = scrapy.Field()
     property_remarks = scrapy.Field()
-    property_show_directions = scrapy.Field(default=None)
+    property_show_directions = scrapy.Field(default="")
     property_contact1_name = scrapy.Field(default='Sales representative')
     property_contact1_phone = scrapy.Field(default='(904) 831 8050')
-    property_contact1_phone_alt = scrapy.Field(default=None)
-    property_contact2_name = scrapy.Field(default=None)
-    property_contact2_phone = scrapy.Field(default=None)
-    property_contact2_phone_alt = scrapy.Field(default=None)
+    property_contact1_phone_alt = scrapy.Field(default="")
+    property_contact2_name = scrapy.Field(default="")
+    property_contact2_phone = scrapy.Field(default="")
+    property_contact2_phone_alt = scrapy.Field(default="")
     property_contact_email = scrapy.Field(default='newhomes@sihomesfl.com')
-    property_driving_directions = scrapy.Field(default=None)
+    property_driving_directions = scrapy.Field(default="")
     property_baths = scrapy.Field()
     property_half_baths = scrapy.Field(default=0)
     property_beds = scrapy.Field()
     property_living = scrapy.Field()
     property_dining = scrapy.Field()
-    property_other_rooms = scrapy.Field(default=None)
+    property_other_rooms = scrapy.Field(default="")
     property_stories = scrapy.Field()
-    property_master = scrapy.Field(default=None)
+    property_master = scrapy.Field(default="")
     property_garage = scrapy.Field()
-    property_school_district_leaid = scrapy.Field(default = [None,None])
-    property_schools_ncesid = scrapy.Field(default = [None,None])
+    property_school_district_leaid = scrapy.Field(default = ["",""])
+    property_schools_ncesid = scrapy.Field(default = ["",""])
     property_square_feet = scrapy.Field()
-    lot_size = scrapy.Field(default=None)
-    lot_description = scrapy.Field(default=None)
-    floor_plan_number = scrapy.Field(default=None)
-    floor_plan_name = scrapy.Field(default=None)
+    lot_size = scrapy.Field(default="")
+    lot_description = scrapy.Field(default="")
+    floor_plan_number = scrapy.Field(default="")
+    floor_plan_name = scrapy.Field(default="")
     propertycommunity_type_id = scrapy.Field(default=11)
-    property_virtual_tour = scrapy.Field()
+    property_virtual_tour = scrapy.Field(default = "")
     property_plan_view_url = scrapy.Field()
-    property_school_comments = scrapy.Field(default=None)
-    property_has_hoa = scrapy.Field(default=True)
-    property_hoa_fee = scrapy.Field(default=None)
-    property_hoa_billing_period = scrapy.Field(default=None)
+    property_school_comments = scrapy.Field(default="")
+    property_has_hoa = scrapy.Field(default="True")
+    property_hoa_fee = scrapy.Field(default="")
+    property_hoa_billing_period = scrapy.Field(default="")
     property_floor_plan_image = scrapy.Field(default=PropertyFloorPlanImage())
     property_elevation_image = scrapy.Field(default=PropertyElevationImage())
     property_exterior_interior_image = scrapy.Field()
     promotion = scrapy.Field(default=Promotion())
 
+    def initialize_defaults(self):
+        for field, metadata in self.fields.items():
+            if 'default' in metadata:
+                self.setdefault(field, metadata['default'])
+
 class Subdivision(scrapy.Item):
     subdivision_name = scrapy.Field()
     subdivision_state_code = scrapy.Field()
     subdivision_city_name = scrapy.Field()
-    subdivision_number = scrapy.Field(default=None)
+    subdivision_number = scrapy.Field(default="")
     community_status_type_code = scrapy.Field()
-    community_min_price = scrapy.Field(default=None)
-    community_max_price = scrapy.Field(default=None)
+    community_min_price = scrapy.Field(default="")
+    community_max_price = scrapy.Field(default="")
     subdivision_zip = scrapy.Field()
     subdivision_address = scrapy.Field()
     builder_brand_name = scrapy.Field(default='SI Homes')
@@ -116,35 +161,45 @@ class Subdivision(scrapy.Item):
     subdivision_longitude = scrapy.Field()
     subdivision_contact1_name = scrapy.Field(default='Sales representative')
     subdivision_contact1_phone = scrapy.Field(default='(904) 831 8050')
-    subdivision_contact1_phone_alt = scrapy.Field(default=None)
-    subdivision_contact2_name = scrapy.Field(default=None)
-    subdivision_contact2_phone = scrapy.Field(default=None)
-    subdivision_contact2_phone_alt = scrapy.Field(default=None)
+    subdivision_contact1_phone_alt = scrapy.Field(default="")
+    subdivision_contact2_name = scrapy.Field(default="")
+    subdivision_contact2_phone = scrapy.Field(default="")
+    subdivision_contact2_phone_alt = scrapy.Field(default="")
     subdivision_contact_email = scrapy.Field(default='newhomes@sihomesfl.com')
-    subdivision_description = scrapy.Field(default=None)
-    subdivision_show_directions = scrapy.Field(default=None)
-    subdivision_driving_directions = scrapy.Field(default=None)
+    subdivision_description = scrapy.Field(default="")
+    subdivision_show_directions = scrapy.Field(default="")
+    subdivision_driving_directions = scrapy.Field(default="")
     subdivision_property_type_id = scrapy.Field()
     subdivision_community_type_id = scrapy.Field(default=11)
-    subdivision_school_district_leaid = scrapy.Field(default=[None,None])
-    subdivision_schools_ncesid = scrapy.Field(default=[None,None])
-    subdivision_school_comments = scrapy.Field(default=None)
-    subdivision_has_hoa = scrapy.Field(default=True)
-    subdivision_hoa_fee = scrapy.Field(default=None)
-    subdivision_hoa_billing_period = scrapy.Field(default=None)
+    subdivision_school_district_leaid = scrapy.Field(default=["",""])
+    subdivision_schools_ncesid = scrapy.Field(default=["",""])
+    subdivision_school_comments = scrapy.Field(default="")
+    subdivision_has_hoa = scrapy.Field(default="True")
+    subdivision_hoa_fee = scrapy.Field(default="")
+    subdivision_hoa_billing_period = scrapy.Field(default="")
     subdivision_notes = scrapy.Field(default="This is a note")
-    subdivision_website = scrapy.Field(default=None)
+    subdivision_website = scrapy.Field(default="")
     subdivision_property = scrapy.Field()
     subdivision_flyer = scrapy.Field(default=SubdivisionFlyer())
     sub_image = scrapy.Field(default=SubImage())
     sub_video = scrapy.Field(default=SubVideo())
     promotion = scrapy.Field(default=Promotion())
 
+    def initialize_defaults(self):
+        for field, metadata in self.fields.items():
+            if 'default' in metadata:
+                self.setdefault(field, metadata['default'])
+
 class Builder(scrapy.Item):
     coop_rate = scrapy.Field(default=CoopRate())
     subdivision = scrapy.Field()
     xmlns_p1 = scrapy.Field(default='SubdivisionSchema')
     xmlns_xsi = scrapy.Field(default = 'http://www.w3.org/2001/XMLSchema-instance')
+
+    def initialize_defaults(self):
+        for field, metadata in self.fields.items():
+            if 'default' in metadata:
+                self.setdefault(field, metadata['default'])
 
 class Welcome10(scrapy.Item):
     builder = scrapy.Field()
--- homescraper/pipelines.py
+++ homescraper/pipelines.py
@@ -1,5 +1,7 @@
 from itemadapter import ItemAdapter
-import json
+import xml.etree.ElementTree as ET
+import ast
+from json2xml import json2xml
 import homescraper.items 
 class HomescraperPipeline:
     def __init__(self):
@@ -21,6 +23,11 @@ class HomescraperPipeline:
     
     def close_spider(self,spider):
         Builder = homescraper.items.Builder()
+        Builder.initialize_defaults()
+        coop_rate = homescraper.items.CoopRate()
+        coop_rate.initialize_defaults()
+        Builder['coop_rate'] =coop_rate
+
         Builder['subdivision'] = []
         for item in self.items:
             i=0
@@ -30,6 +37,20 @@ class HomescraperPipeline:
                     break
             if i == 0:
                 Subdivision = homescraper.items.Subdivision()
+                Subdivision.initialize_defaults()
+                SubdivisionFlyer = homescraper.items.SubdivisionFlyer()
+                SubdivisionFlyer.initialize_defaults()
+                SubImage = homescraper.items.SubImage()
+                SubImage.initialize_defaults()
+                SubVideo = homescraper.items.SubVideo()
+                SubVideo.initialize_defaults()
+                Promotion = homescraper.items.Promotion()
+                Promotion.initialize_defaults()
+                Subdivision['subdivision_flyer'] = SubdivisionFlyer
+                Subdivision['sub_image'] = SubImage
+                Subdivision['sub_video'] = SubVideo
+                Subdivision['promotion'] = Promotion
+
                 Subdivision['subdivision_name'] = item['address_map']['City']
                 Subdivision['subdivision_state_code'] = item['address_map']['State']
                 Subdivision['subdivision_city_name'] = item['address_map']['City']
@@ -50,6 +71,17 @@ class HomescraperPipeline:
         
         for item in self.items:
             SubdivisionProperty = homescraper.items.SubdivisionProperty()
+            SubdivisionProperty.initialize_defaults()
+            PropertyFloorPlanImage = homescraper.items.PropertyFloorPlanImage()
+            PropertyFloorPlanImage.initialize_defaults()
+            PropertyElevationImage = homescraper.items.PropertyElevationImage()
+            PropertyElevationImage.initialize_defaults()
+            Promotion = homescraper.items.Promotion()
+            Promotion.initialize_defaults()
+            SubdivisionProperty['promotion'] = Promotion
+            SubdivisionProperty['property_floor_plan_image'] = PropertyFloorPlanImage
+            SubdivisionProperty['property_elevation_image'] = PropertyElevationImage
+
             SubdivisionProperty['property_address'] = item['address_map']['address']
             SubdivisionProperty['property_zip'] = item['address_map']['Zip_Code']
             SubdivisionProperty['property_latitude'] = item['address_map']['latitude']
@@ -70,26 +102,38 @@ class HomescraperPipeline:
             SubdivisionProperty['property_remarks'] = '. '.join(item["features"])
             SubdivisionProperty['property_baths'] = item["basic_details"]["Bathrooms"]
             SubdivisionProperty['property_beds'] = item["basic_details"]["Bedrooms"]
-            SubdivisionProperty['property_living'] =None
-            SubdivisionProperty['property_dining'] = None
-            SubdivisionProperty['property_stories'] = None
             SubdivisionProperty['property_garage'] = item["basic_details"]["Garage"]
-            SubdivisionProperty['property_schools_ncesid'] = None
             SubdivisionProperty['property_square_feet'] = item["basic_details"]["Square_Footage"]
-            SubdivisionProperty['property_virtual_tour'] = item['video_link']
+            if item['video_link'] is not None:
+                SubdivisionProperty['property_virtual_tour'] = item['video_link']
+            else:
+                SubdivisionProperty['property_virtual_tour'] = ""
             SubdivisionProperty['property_plan_view_url'] = item['blueprint'][0]
             SubdivisionProperty['property_exterior_interior_image'] = []
             for image in item['images']:
                Image =  homescraper.items.PropertyExteriorInteriorImage()
+               Image.initialize_defaults()
                Image['property_interior_image_url'] = image
                SubdivisionProperty['property_exterior_interior_image'].append(Image)
             for subdivision in Builder['subdivision']:
                  if item['address_map']['City'] == subdivision['subdivision_name']:
                     subdivision['subdivision_property'].append(SubdivisionProperty)
                     break
-            print(item['basic_details'].keys())
-        
-
+        welcome = homescraper.items.Welcome10()
+        welcome['builder'] = Builder
+        dt = ast.literal_eval(str(welcome))
+        xml = json2xml.Json2xml(dt,item_wrap=False).to_xml()
+        root = ET.fromstring(xml)
+        xml = list(root)[0]
+        def remove_type_attributes(element):
+            if 'type' in element.attrib:
+                del element.attrib['type']
+            for child in element:
+                remove_type_attributes(child)
+        remove_type_attributes(xml)
+        xml = ET.tostring(xml, encoding='unicode')
+        with open('data1.xml','w') as f:
+            f.write(xml)
         pass
 
 
